d78074f86a23b6b061ee95eeeaf5660ec3aeb5fd
k1z1n <martynm1314@gmail.com>
2024-02-12 21:47:26 +0300

    dynamic example

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$request_uri = $_SERVER['REQUEST_URI'];
+$request_uri = strtok($request_uri, '?');
+$uri_parts = explode('/', $request_uri);
+
+array_shift($uri_parts);
+$controller = $uri_parts[0] ?? '';
+include 'includes/header.php';
+switch ($controller) {
+    case '':
+        include('views/main.php');
+        break;
+    case '404':
+        include('views/404.php');
+        break;
+    default:
+        http_response_code(404);
+        include('views/404.php');
+        break;
+}
+include "includes/footer.php";
